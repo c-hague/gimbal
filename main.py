4a42fe1955d788fdbc4cc1c3f05824b1b08e83ea
Collin Hague <chague@uncc.edu>
2021-11-24 16:58:15 +0000

    face detection working

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import cv2
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 import time
+import logging
 
-
-TEMP_FILE = 'temp.jgp'
+TEMP_FILE = 'temp.jpg'
 
 
 def main():
@@ -12,25 +12,25 @@ def main():
     camera.resolution = (640, 480)
     camera.framerate = 32
     rawCapture = PiRGBArray(camera, size=(640, 480))
-    faces = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     # allow the camera to warmup
     time.sleep(0.1)
     # capture frames from the camera
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        
-        gray = cv2.cvtColor(frame, cv2.COLOR_BRG2GRAY)
-        found = faces.detectMultiScale(frame, minSize =(20, 20))        
+        img = frame.array
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        found = faces.detectMultiScale(gray, minSize =(20, 20))
         if len(found) != 0:
             x, y, width, height = found[0]
-            cv2.rectangle(frame, (x, y), 
+            cv2.rectangle(img, (x, y), 
                         (x + height, y + width), 
                         (0, 255, 0), 5)
-            cv2.imwrite(TEMP_FILE, frame)
+        print('found {0} faces {1}'.format(len(found), time.time()))
+        cv2.imwrite(TEMP_FILE, img)
 
-        # wait 1ms for key to see if quitting is needed
-        key = cv2.waitKey(1) & 0xFF
         # clear the stream in preparation for the next frame
         rawCapture.truncate(0)
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
+
+
+if __name__ == '__main__':
+    main()
